852c8fa5d53f9e7530cacd8a0c52fcebd2c12515
liren <327381633@qq.com>
2025-09-07 17:40:34 +0800

    project1: update gitignore

--- projects/1-ai-powered-job-prep-main/drizzle.config.ts
+++ projects/1-ai-powered-job-prep-main/drizzle.config.ts
@@ -1,11 +1,14 @@
-import { env } from "@/data/env/server"
-import { defineConfig } from "drizzle-kit"
+import { env } from "@/data/env/server";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
+  // 指定数据库迁移文件的输出目录
   out: "./src/drizzle/migrations",
   schema: "./src/drizzle/schema.ts",
+  // 告诉 Drizzle 使用 PostgreSQL 特定的 SQL 语法
   dialect: "postgresql",
+  // 数据库连接配置
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-})
+});
